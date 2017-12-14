217c5fd41e0bdd602631ee9ccdc1c252d189f82c
pedrolandaacurio <pedrolandaacurio@hotmail.com>
2017-12-14 00:27:11 -0500

    Agregar archivos de proyecto.

---
+++ SitiosWebForms/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SitiosWebForms.Startup))]
+namespace SitiosWebForms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
